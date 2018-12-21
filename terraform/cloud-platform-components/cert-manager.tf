fe70e2f67a188b2b932a6310fcbdbdb2da55d5d6
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2018-12-21 10:03:53 +0000

    add letsencrypt

--- terraform/cloud-platform-components/cert-manager.tf
+++ terraform/cloud-platform-components/cert-manager.tf
@@ -2,48 +2,6 @@ variable "cert-manager-ns" {
   default = "cert-manager"
 }
 
-resource "kubernetes_service_account" "cert-manager-tiller" {
-  metadata {
-    name      = "tiller"
-    namespace = "${var.cert-manager-ns}"
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "cert-manager-tiller" {
-  metadata {
-    name = "cert-manager-tiller"
-  }
-
-  role_ref {
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-    api_group = "rbac.authorization.k8s.io"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = "tiller"
-    namespace = "${var.cert-manager-ns}"
-    api_group = ""
-  }
-}
-
-resource "null_resource" "cert-manager-tiller" {
-  depends_on = [
-    "kubernetes_service_account.cert-manager-tiller",
-    "kubernetes_cluster_role_binding.cert-manager-tiller",
-  ]
-
-  provisioner "local-exec" {
-    command = "helm --kube-context ${terraform.workspace} --tiller-namespace ${var.cert-manager-ns} init --wait --service-account tiller"
-  }
-
-  provisioner "local-exec" {
-    when    = "destroy"
-    command = "kubectl --context ${terraform.workspace} -n ${var.cert-manager-ns} delete deployment.apps/tiller-deploy service/tiller-deploy"
-  }
-}
-
 resource "helm_release" "cert-manager" {
   name      = "cert-manager"
   chart     = "stable/cert-manager"
@@ -54,8 +12,23 @@ resource "helm_release" "cert-manager" {
     name  = "image.tag"
     value = "v0.5.2"
   }
+  
+  set {
+    name = "ingressShim.defaultIssuerName"
+    value = "letsencrypt-staging"
+  }
 
-  depends_on = ["null_resource.cert-manager-tiller"]
+  set {
+    name = "ingressShim.defaultIssuerKind"
+    value = "ClusterIssuer"
+  }
+
+  set {
+    name = "createCustomResource"
+    value = "false"
+  }
+
+  depends_on = ["null_resource.deploy"]
 
   lifecycle {
     ignore_changes = ["keyring"]
